1e61f02f8da89ef2a14e26d95a89078c88e52afa
leticiaoli <lesilvaoliveira03@gmail.com>
2020-04-12 14:02:40 -0300

    desenvolvimento do slide principal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     
+    function mainSlide() {
+        let imgs = $('.mainSlide')
+
+        imgs.slick({
+            dots: true 
+        })
+    }
+
+    mainSlide()
+
 })
