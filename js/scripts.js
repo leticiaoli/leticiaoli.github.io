be5c66df0a15f8a027167ef26b99070d66c3d2df
leticiaoli <lesilvaoliveira03@gmail.com>
2020-04-19 21:20:33 -0300

    Finalização footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     function mainSlide() {
         let imgs = $('.mainSlide')
 
         imgs.slick({
-            dots: true 
+            dots: true
         })
     }
 
+    function infoSlide() {
+        let item = $('.slideInfo')
+
+        item.slick({
+            arrows: false,
+            dots: true
+        })
+    }
+
+    // funcs
     mainSlide()
+    infoSlide()
 
 })
